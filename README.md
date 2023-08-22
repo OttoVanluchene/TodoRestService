11ac4ed63fd9e775354fbbd199f80fb674b07618
v93938 <ovanluchene@vanmarcke.be>
2023-08-22 17:19:39 +0200

    readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# TODO Rest Webservice
+
+## Description
+
+Pyhton FastAI Webserice, a simple TODO webservice to learn FastAI and Python.
+Followed this tutorial: https://www.gormanalysis.com/blog/building-a-simple-crud-application-with-fastapi/
+
+
--- models.py
+++ models.py
@@ -6,7 +6,3 @@ class ToDo(Base):
     __tablename__ = 'todos'
     id = Column(Integer, primary_key=True)
     task = Column(String(256))
-
-# Pydantic model definition
-# Pydantic models are used to define the request and response models for the API endpoints.
-# So DTO (Data Transfer Object)
